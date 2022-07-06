178172ca47883f74253ea8f1c2acf74a01b6003d
Captain Jack <andruf52@gmail.com>
2022-07-06 17:57:36 +0400

    Styles updated, footer component updated.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b2315c95b4a8ad07"; }
+/******/ 		__webpack_require__.h = function() { return "38e28fccebfd97b8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
