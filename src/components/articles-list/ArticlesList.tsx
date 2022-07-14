293c41f1d7999afd2d073ed7fb222038ed861903
Captain Jack <andruf52@gmail.com>
2022-07-14 19:25:00 +0400

    Excess comment removed, import order changed at src/components/articles-list/ArticlesList.tsx

--- src/components/articles-list/ArticlesList.tsx
+++ src/components/articles-list/ArticlesList.tsx
@@ -1,13 +1,12 @@
 import React, { FunctionComponent } from 'react';
+import { useMediaQuery } from 'usehooks-ts';
 import { MEDIA_QUERY_DESKTOP } from 'constants/ui';
 import type { Article as ArticleModel } from 'models/Article';
-import { useMediaQuery } from 'usehooks-ts';
 import { useArticles } from 'hooks/useArticles';
 import { ArticlePreview } from './article-preview/ArticlePreview';
 
 import styles from './styles.module.scss';
 
-/* Return list of articles */
 export const ArticlesList: FunctionComponent = () => {
   const isDesktop = useMediaQuery(MEDIA_QUERY_DESKTOP);
   const { articles, isLoading, isError } = useArticles('home');
