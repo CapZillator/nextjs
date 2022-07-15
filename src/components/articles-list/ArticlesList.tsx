9280b48689191a4ef6f233806d57e9bf77fc1b73
Captain Jack <andruf52@gmail.com>
2022-07-15 14:30:19 +0400

    Import order edited, code formated.

--- src/components/articles-list/ArticlesList.tsx
+++ src/components/articles-list/ArticlesList.tsx
@@ -1,14 +1,13 @@
 import React, { FunctionComponent } from 'react';
 import Link from 'next/link';
+import { useMediaQuery } from 'usehooks-ts';
 import { MEDIA_QUERY_DESKTOP } from 'constants/ui';
 import type { Article as ArticleModel } from 'models/Article';
-import { useMediaQuery } from 'usehooks-ts';
 import { useArticles } from 'hooks/useArticles';
 import { ArticlePreview } from './article-preview/ArticlePreview';
 
 import styles from './styles.module.scss';
 
-/* Return list of articles */
 export const ArticlesList: FunctionComponent = () => {
   const isDesktop = useMediaQuery(MEDIA_QUERY_DESKTOP);
   const { articles, isLoading, isError } = useArticles('home');
@@ -32,6 +31,7 @@ export const ArticlesList: FunctionComponent = () => {
           <Link
             key={article.short_url}
             href={`/article/${article.uri.split('/').pop()}`}
+            passHref
           >
             <a>
               <ArticlePreview article={article} isDesktop={isDesktop} />
