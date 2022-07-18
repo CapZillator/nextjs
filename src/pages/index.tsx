a2f6f09e6097ee6f6509a8f900793e63fd37807e
Captain Jack <andruf52@gmail.com>
2022-07-18 20:39:53 +0400

    SSR on ArticlesList and Article implemented, useMediqQuery hook replaced by useMediaQuerySSR.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,32 @@
 import type { NextPage } from 'next';
-import { SWRConfig, SWRConfiguration } from 'swr';
+import { GetServerSideProps } from 'next';
 import { ArticlesList } from 'components/articles-list/ArticlesList';
 import { articlesFetcher } from 'service/articlesFetcher';
 
-export const Home: NextPage<SWRConfiguration> = ({ fallback }) => {
-  return <SWRConfig value={{ fallback }}>
-    <ArticlesList />
-  </SWRConfig>;
+export const Home: NextPage = () => {
+  return <ArticlesList />;
 };
 
-export async function getServerSideProps() {
-  const articles = await articlesFetcher('home');
 
-  return {
-    props: {
-      fallback: {
-        'home': articles
-      }
+export const getServerSideProps: GetServerSideProps = async () => {
+  try {
+    const articles = await articlesFetcher('home');
+
+    if (!articles) {
+      return { notFound: true };
     }
-  }
+
+    return {
+      props: {
+        fallback: {
+          'home': articles
+        }
+      }
+    };
+  } catch (e) {
+    return { notFound: true };
+  };
+  
 }
 
 export default Home;
